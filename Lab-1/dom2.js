4c7e645795fcebd6fc1b4fad611b4d1c4c528140
adityamishra0706 <adityamishra07062005@gmail.com>
2026-07-30 11:15:57 +0530

    dom Starting

---
+++ Lab-1/dom2.js
@@ -0,0 +1,10 @@
+// DOM --> Document Object Model
+import { EventEmitter } from "events";
+const button = new EventEmitter();
+button.on("click", () => {
+  console.log("Task 1");
+});
+button.on("click", () => {
+  console.log("Task 2");
+});
+button.emit("click");
